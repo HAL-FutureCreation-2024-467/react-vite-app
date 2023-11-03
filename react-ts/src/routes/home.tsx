c959d8f114936788501797d371cb8671e54d782d
Hepha0014 <gkw999moabc@yahoo.co.jp>
2023-11-03 20:18:09 +0900

    home.tsxでのprofile取得

--- react-ts/src/routes/home.tsx
+++ react-ts/src/routes/home.tsx
@@ -1,25 +1,68 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";   
+import type { Database } from '../types/database'
 
-// const [sessions, setSession] = useState(null)
+//const user = supabase.auth.user()
 
-// useEffect(() => {
-//     supabase.auth.getSession().then(({ data: { session } }) => {
-//     setSession(session)
-// })
-
-//     supabase.auth.onAuthStateChange((_event, session) => {
-//       setSession(session)
-//     })
-//   }, [])
-
-// const user = supabase.auth.user()
+type userprofiles = {
+    avatar_url: string | null;
+    full_name: string | null;
+    id: string;
+    updated_at: string | null;
+    username: string | null;
+    website: string | null;
+} | null
 
 const Home = () => {
+    const [sessions, setSession] = useState()
+    const [user, setUser] = useState<userprofiles>()
+    //できればこの下の記述を使いたい database.tsの型を使用したい
+    //const [user, setUser] = useState<Database>()
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+        setSession(session)
+    })
+
+        supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session)
+        })
+    }, [])
+
+
+    useEffect(() => {
+        const setupUser = async () => {
+            if(sessions?.user.id){
+                //console.log(sessions.user.id)
+                const {data} = await supabase
+                    .from('profiles')
+                    .select('*')
+                    .eq('id', sessions.user.id)
+                    .single()
+                //console.log("user?" + profiles.username)
+                setUser(data)
+                //console.log("user?" + user.username)
+            }
+        }
+        setupUser()
+    },[sessions])
+
     return(
+        <>
         <div>
-            <h1>Home</h1>
+            <section>
+            <div>
+                <p>レベル</p>
+            </div>
+            <div>
+                <p>{user && user.username}</p>
+                <p>アイテム数</p>
+            </div>
+            <div>
+                <p>モーダルを表示</p>
+            </div>
+            </section>
         </div>
+        </>
     );
 }
 
