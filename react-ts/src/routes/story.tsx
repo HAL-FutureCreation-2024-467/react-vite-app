ebb4edcc4f449982cdaf91e1a4e1e1f833b85bf5
kawano.terumi <precept.terumi.web@gmail.com>
2023-11-03 15:43:18 +0900

    update DB schema and story.tsx

--- react-ts/src/routes/story.tsx
+++ react-ts/src/routes/story.tsx
@@ -1,7 +1,53 @@
-function story() {
+// import "@scss/story.scss";
+import { useState, useEffect } from "react";
+import { supabase } from "../supabaseClient";
+import { Json } from "../types/database";
+
+const story = () => {
+  const TABLE_NAME = "story";
+  const [storyDB, setStory] = useState<Json>(null); // 初期状態を null に変更
+
+  const getImage = (filePath: string): string => {
+    return new URL(`../assets/${filePath}`, import.meta.url).href;
+  };
+
+  const fetchStory = async () => {
+    try {
+      const { data, error } = await supabase.from(TABLE_NAME).select('sentence').eq('chapter', 1).eq('paragraph', 1);
+      if (error) {
+        console.error(error);
+      } else if (data !== null && data.length > 0) {
+        setStory(data);
+      } else {
+        console.error("No data found in the response.");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
+  useEffect(() => {
+    fetchStory();
+  }, []);
+  
+  // chapter 章
+  // paragraph 段落
+  // sentence 文
+
+  // storyDB ステートが null の場合にローディングを表示
+  if (storyDB === null) {
+    return (
+    <div>
+      <p>Loading...</p>
+    </div>
+    )
+  }
+
   return (
     <div>
-      <h1>Story</h1>
+      <p>{storyDB.toString()}</p>
     </div>
   );
 }
