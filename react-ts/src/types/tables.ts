703af32fcd92335e9767d9b9b7970016208ef498
kawano.terumi <precept.terumi.web@gmail.com>
2023-11-04 11:33:16 +0900

    database ts and typesfile create

---
+++ react-ts/src/types/tables.ts
@@ -0,0 +1,9 @@
+import {Database, Json} from "./database";
+
+export type StoryType = Database["public"]["Tables"]["story"]["Row"]
+  
+export type MonsterType = Database["public"]["Tables"]["monsters"]["Row"]
+
+export type HighscoreType = Database["public"]["Tables"]["highscore"]["Row"]
+
+export type ProfileType = Database["public"]["Tables"]["profiles"]["Row"]
