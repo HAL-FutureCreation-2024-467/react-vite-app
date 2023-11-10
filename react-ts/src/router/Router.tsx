93102b7ed524ec53bbf68faee77d0dfdcd7ba9ac
ishi-arata <arata.contact.web@gmail.com>
2023-11-11 02:06:04 +0900

    Router.tsxのstory部分を修正

--- react-ts/src/router/Router.tsx
+++ react-ts/src/router/Router.tsx
@@ -16,7 +16,7 @@ export const Router: FC = () => {
       {/* <Route path="/account" element{< />}></Route> */}
       {/* <Route path="/setting" element={<Setting />} /> */}
       <Route path="/home" element={<Home />} />
-      <Route path="/story/:chapter=:paragraph?" element={<Story />} />
+      <Route path="/story/:chapter?/:paragraph?" element={<Story />} />
       <Route path="/game/practice/:id?" element={<Game_p />} />
       <Route path="/game/test/:id?" element={<Game_t />} />
       <Route path="*" element={<NotF />} />
