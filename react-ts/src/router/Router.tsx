78c99e160365cc68113c96fd584dc2f3f71d69f5
Hepha0014 <gkw999moabc@yahoo.co.jp>
2023-11-11 03:08:38 +0900

    homeとユーザ情報登録画面の分離

--- react-ts/src/router/Router.tsx
+++ react-ts/src/router/Router.tsx
@@ -6,11 +6,13 @@ import Story from "../routes/story";
 import Game_p from "../routes/game_prac";
 import Game_t from "../routes/game_test";
 import NotF from "../routes/404"; 
+import RegForm from "../routes/RegForm";
 
 export const Router: FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Index />} />
+      <Route path="/RegForm" element={<RegForm />} />
       <Route path="/home" element={<Home />} />
       <Route path="/story/:chapter=:paragraph?" element={<Story />} />
       <Route path="/game/practice/:id?" element={<Game_p />} />
--- react-ts/src/routes/home.tsx
+++ react-ts/src/routes/home.tsx
@@ -7,7 +7,7 @@ import StoryTab from "../components/home/StoryTab";
 import Avatar from '../components/Avatar'
 import { error } from "console";
 import HomeModal from "../components/home/HomeModal";
-import { json } from "stream/consumers";
+import { useNavigate } from 'react-router-dom';
 
 
 const Home = () => {
@@ -18,16 +18,11 @@ const Home = () => {
         'quiz': false,
         'story': false
     })
-    const [loading, setLoading] = useState(true)
-    const [username, setUsername] = useState(null)
-    const [full_name, setFullName] = useState(null)
-    const [website, setWebsite] = useState(null)
-    const [avatar_url, setAvatarUrl] = useState(null)
-    const [exp, setExp] = useState<number>(0)
+    
+    const navigate = useNavigate();
     const [showConfigModal, setShowModal] = useState(false);
     const [menuBar, setMenu] = useState("line_menu.png");
 
-    //const [exp, setExp] = useState<number>(0)
     useEffect(() => {
         supabase.auth.getSession().then(({ data: { session } }) => {
             setSession(session)
@@ -37,7 +32,6 @@ const Home = () => {
             setSession(session)
         })
     }, [])
-
 
     const setTab = (tabName: string) => {
         const updatedTabs: { [key: string]: boolean } = {};
@@ -65,47 +59,16 @@ const Home = () => {
         setupUser()
     }, [sessions])
 
-
-    async function updateProfile(event, avatarUrl) {
-        event.preventDefault()
-        const setupUser = async () => {
-            if (sessions?.user.id) {
-                const { data: profiles, error } = await supabase
-                    .from('profiles')
-                    .select('*')
-                    .eq('id', sessions.user.id)
-                if (error) {
-                    console.error("データの取得に失敗しました", error);
-                } else {
-                    console.log("データの取得に成功しました", profiles);
-                    setUser(profiles[0])
-                }
+    useEffect(() => {
+        const navigateRegFrom = async () => {
+            if (user?.username === null) {
+                navigate('/RegForm');
+            } else {
+                console.log(user?.username);
             }
-        }
-
-        setLoading(true)
-
-        const updates = {
-            avatar_url: avatarUrl,
-            exp: 0,
-            full_name: full_name,
-            id: user?.id,
-            game_state: user?.game_state,
-            updated_at: new Date(),
-            username: username,
-            website: website,
-        }
-
-        const { error } = await supabase.from('profiles').upsert(updates)
-
-        if (error) {
-            alert(error.message)
-        } else {
-            setAvatarUrl(avatar_url)
-            setupUser()
-        }
-        setLoading(false)
-    }
+        };
+        navigateRegFrom();
+    }, [user]);
 
 
     const toggleModal = () => {
@@ -116,7 +79,6 @@ const Home = () => {
         } else {
             setMenu('line_menu.png');
         }
-
     };
 
     const getImage = (filePath: string): string => {
@@ -125,106 +87,107 @@ const Home = () => {
 
     return (
         <>
+          {user?.username ? (
             <div className="App">
-                <div className={"black"}></div>
-                <section className={"home-wrap"}>
-                    <section>
-                        <div>
-                            <p>レベル</p>
-                        </div>
-                        <div>
-                            <p>{user && user.username}</p>
-                            <p>アイテム数</p>
-                        </div>
-                        <div>
-                            <p>モーダルを表示</p>
-                        </div>
-                    </section>
-                    <div className={showConfigModal ? "overlay-add" : "overlay"}>
-                        <HomeModal />
-                    </div>
-                    {user?.username ? (
-                        showTab['home'] ? (
-                            <>
-                                <h1>ホーム</h1>
-                            </>
-                        ) : showTab['quiz'] ? (
-                            // Quiz コンポーネントの内容
-                            <>
-                                <h1>クイズ</h1>
-                                <QuizTab />
-                            </>
-                        ) : showTab['story'] ? (
-                            // Story コンポーネントの内容
-                            <>
-                                <h1>ストーリー</h1>
-                                <StoryTab />
-                            </>
-                        ) : null
-                    ) : (
-                        // ユーザー情報登録画面の内容
-                        <>
-                            <p>ユーザー情報登録画面</p>
-                            <form onSubmit={updateProfile} className="form-widget">
-                                <Avatar
-                                    url={avatar_url}
-                                    size={150}
-                                    onUpload={(event, url) => {
-                                        updateProfile(event, url)
-                                    }}
-                                />
-                                <div>
-                                    <label htmlFor="email">Email</label>
-                                    <input id="email" type="text" value={sessions?.user.email} disabled />
-                                </div>
-                                <div>
-                                    <label htmlFor="username">Name</label>
-                                    <input
-                                        id="username"
-                                        type="text"
-                                        required
-                                        value={username || ''}
-                                        onChange={(e) => setUsername(e.target.value)}
-                                    />
-                                </div>
-                                <div>
-                                    <label htmlFor="full_name">FullName</label>
-                                    <input
-                                        id="full_name"
-                                        type="text"
-                                        required
-                                        value={full_name || ''}
-                                        onChange={(e) => setFullName(e.target.value)}
-                                    />
-                                </div>
-                                <div>
-                                    <label htmlFor="website">Website</label>
-                                    <input
-                                        id="website"
-                                        type="url"
-                                        value={website || ''}
-                                        onChange={(e) => setWebsite(e.target.value)}
-                                    />
-                                </div>
-                                <div>
-                                    <button className="button block primary"
-                                        type="submit"
-                                        disabled={loading}
-                                    >
-                                        {loading ? 'Loading ...' : 'Update'}
-                                    </button>
-                                </div>
-                            </form>
-                        </>
-                    )}
+              <div className={"black"}></div>
+              <section className={"home-wrap"}>
+                <section>
+                  <div>
+                    <p>レベル</p>
+                  </div>
+                  <div>
+                    <p>{user && user.username}</p>
+                    <p>アイテム数</p>
+                  </div>
+                  <div>
+                    <p>モーダルを表示</p>
+                  </div>
                 </section>
-                <div>
-                    <button onClick={() => setTab('home')}>ホーム</button>
-                    <button onClick={() => setTab('quiz')}>クイズ</button>
-                    <button onClick={() => setTab('story')}>ストーリー</button>
+                <div className={showConfigModal ? "overlay-add" : "overlay"}>
+                  <HomeModal />
                 </div>
+                {showTab['home'] ? (
+                  <h1>ホーム</h1>
+                ) : showTab['quiz'] ? (
+                  <>
+                    <h1>クイズ</h1>
+                    <QuizTab />
+                  </>
+                ) : showTab['story'] ? (
+                  <>
+                    <h1>ストーリー</h1>
+                    <StoryTab />
+                  </>
+                ) : null}
+              </section>
+              <div>
+                <button onClick={() => setTab('home')}>ホーム</button>
+                <button onClick={() => setTab('quiz')}>クイズ</button>
+                <button onClick={() => setTab('story')}>ストーリー</button>
+              </div>
             </div>
+          ) : null}
         </>
-    );
+      );
+      
+      
+      
+      
+      
+      
+    // return (
+    //     <>
+    //         <div className="App">
+    //             <div className={"black"}></div>
+    //             <section className={"home-wrap"}>
+    //                 <section>
+    //                     <div>
+    //                         <p>レベル</p>
+    //                     </div>
+    //                     <div>
+    //                         <p>{user && user.username}</p>
+    //                         <p>アイテム数</p>
+    //                     </div>
+    //                     <div>
+    //                         <p>モーダルを表示</p>
+    //                     </div>
+    //                 </section>
+    //                 <div className={showConfigModal ? "overlay-add" : "overlay"}>
+    //                     <HomeModal />
+    //                 </div>
+    //                 {user?.username ? (
+
+                        
+    //                     showTab['home'] ? (
+    //                         <>
+    //                             <h1>ホーム</h1>
+    //                         </>
+    //                     ) : showTab['quiz'] ? (
+    //                         // Quiz コンポーネントの内容
+    //                         <>
+    //                             <h1>クイズ</h1>
+    //                             <QuizTab />
+    //                         </>
+    //                     ) : showTab['story'] ? (
+    //                         // Story コンポーネントの内容
+    //                         <>
+    //                             <h1>ストーリー</h1>
+    //                             <StoryTab />
+    //                         </>
+    //                     ) : null
+    //                 ) : (
+    //                     <>
+                        
+    //                     </>
+    //                 )}
+    //             </section>
+    //             <div>
+    //                 <button onClick={() => setTab('home')}>ホーム</button>
+    //                 <button onClick={() => setTab('quiz')}>クイズ</button>
+    //                 <button onClick={() => setTab('story')}>ストーリー</button>
+    //             </div>
+    //         </div>
+    //     </>
+    // );
 }
 export default Home;
