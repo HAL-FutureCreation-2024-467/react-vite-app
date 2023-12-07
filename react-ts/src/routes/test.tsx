24676ecbd47c8e74d0624ad17fdff4a99b071911
kawano.terumi <precept.terumi.web@gmail.com>
2023-12-07 16:02:33 +0900

    haa

--- react-ts/src/routes/test.tsx
+++ react-ts/src/routes/test.tsx
@@ -1,23 +1,25 @@
 import React, { useRef } from 'react';
-import Live2DModule from '../components/Live2D/Live2d-slime';
+import Live2DModule from '../components/Live2D/Live2dModuleTest';
 
 const Test = () => {
     const modelPath = '/Live2dModel/slime/silme.model3.json';
+    const modelRutica = '/Live2dModel/rutika/rutika.model3.json';
     const childRef = useRef<any>(null);
-    const playRush = () => {childRef.current ? childRef.current.rush() : null};
-    const playSecond = () => {childRef.current ? childRef.current.second() : null};
-    const playThree = () => {childRef.current ? childRef.current.three() : null};
-    const playFinal = () => {childRef.current ? childRef.current.final() : null};
+    const playRush = () => {childRef.current.rush()};
+    const slash = () => {childRef.current ? childRef.current.slash() : null};
+    const second = () => {childRef.current ? childRef.current.second() : null};
+    const three = () => {childRef.current ? childRef.current.three() : null};
+    const final = () => {childRef.current ? childRef.current.final() : null};
 
     
     return (
         <>
             <div className='App'>
-                <Live2DModule ref={childRef} modelPath={modelPath as string} />
-                <button onClick={playRush} value={1}>motion1</button>
-                <button onClick={playSecond} value={2}>motion2</button>
-                <button onClick={playThree} value={3}>motion3</button>
-                <button onClick={playFinal} value={4}>motion4</button>
+                <Live2DModule ref={childRef} modelPath={modelRutica as string} />
+                <button onClick={slash} value={1}>motion1</button>
+                <button onClick={second} value={2}>motion2</button>
+                <button onClick={three} value={3}>motion3</button>
+                <button onClick={final} value={4}>motion4</button>
             </div>
         </>
     )
